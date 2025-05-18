9a9ca2b05cbf3621cee0884d00c44cc2b482897d
dianras <nraiyshdd@gmail.com>
2025-05-18 20:07:03 +0700

    Update RiwayatPenghuni.java

--- ClassRiwayatPenghuni/RiwayatPenghuni.java
+++ ClassRiwayatPenghuni/RiwayatPenghuni.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ClassRiwayatPenghuni;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
-
-/**
- *
- * @author Rilda Dwi Febrianda
- */
 
 @Entity
 @Table(name = "riwayat_penghuni")
